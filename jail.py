249a4cf0abafb36f5330d360281cd1c6bd04abbf
jonathanportillo <jonathanportillor@berkeley.edu>
2021-04-20 20:47:14 -0500

    added json

--- jail.py
+++ jail.py
@@ -1,4 +1,7 @@
 import requests
+import json
 
 response = requests.get("https://www.jailbase.com/api/1/recent/?source_id=az-mcso")
-print(response.text)
+response_dic =json.loads(response.text)
+records = (response_dic['records'])
+print(records[1]['name'])
